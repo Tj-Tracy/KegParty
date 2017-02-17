a9833a1bf8225c44023800a5c4f66d1cadbfb931
Thomas Tracy <Thomas_Tracy@student.uml.edu>
2017-02-17 17:52:00 -0500

    Started experiments with enviroment

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+
+const app = express();
+
+const axios = require('axios');
+
+const brewAPI = '&key=72220654c478e056063a0d4757578df4';
+
+app.set('view engine', 'ejs');
+
+class Beer {
+
+  constructor(name, description, id) {
+    this.name = name;
+    this.description = description;
+    this.id = id;
+  }
+}
+
+const beerList = [];
+
+function populateBeerList(searchData) {
+  searchData.data.forEach((beer) => {
+    const x = new Beer(beer.name, beer.description, beer.id);
+    beerList.push(x);
+  });
+}
+
+app.get('/', (req, res) => {
+  axios.get(`http://api.brewerydb.com/v2/search?q=coors${brewAPI}`)
+  .then((response) => {
+    populateBeerList(response.data);
+    res.render('index', { beerList });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+});
+
+app.get('/beers/:beerid', (req, res) => {
+  console.log(res.params.beerid);
+})
+
+
+app.listen('3000', () => {
+  console.log('Started at 3000');
+});
+
