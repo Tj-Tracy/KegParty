7041eccd8b863ce2c4ef23b72a97739bbab022e2
CJMcLaughlin <Connor_McLaughlin@student.uml.edu>
2017-04-13 20:57:23 -0400

    Help Page
    The first draft of the help page.

---
+++ public/js/home.js
@@ -0,0 +1,21 @@
+
+
+function b1(){
+  document.getElementById("output").innerHTML="Feeling Cold? We Recommend: <a href=\"https://kegparty.herokuapp.com/beers/HEBdhi\">Blackest Night Imperial Stout</a>";
+}
+function b2(){
+  document.getElementById("output").innerHTML="Feeling Hot? We Recommend: <a href=\"https://kegparty.herokuapp.com/beers/XPmifH\">Sam Adams Summer Saison</a>";
+}
+function b3(){
+  document.getElementById("output").innerHTML="Going to a Party? We Recommend: <a href=\"https://kegparty.herokuapp.com/beers/wfsyxi\">Hoegaarden</a>";
+}
+function b4(){
+  document.getElementById("output").innerHTML="Going to a the Beach? We Recommend: <a href=\"https://kegparty.herokuapp.com/beers/NzLeAf\">Whales Tale Pale Ale";
+}
+function b5(){
+  document.getElementById("output").innerHTML="About to have Dinner? We Recommend: <a href=\"https://kegparty.herokuapp.com/beers/FGndg9\">La Fin Du Monde</a> ";
+}
+function b6(){
+  document.getElementById("output").innerHTML="Watching Sports? We Recommend: <a href=\"https://kegparty.herokuapp.com/beers/mGMxmW/\">Molsen Canadian</a> ";
+}
+
